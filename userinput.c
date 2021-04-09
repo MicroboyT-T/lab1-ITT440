fca7091f3bb650c5f8f8bb00083ce4358b75e6ab
MicroboyT-T <muazsolihin11@gmail.com>
2021-04-09 15:51:31 +0000

    second code

---
+++ userinput.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (void){
+/* This is my second program in C*/
+
+int age;
+printf("Hi,how old are you?\n");
+scanf("%d",&age);
+printf("You are %d years old:", age);
+return (0);
+}
